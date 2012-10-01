dbbc214bd220fa44a908d0cf207a3a15343a16a6
Jens W. Klein <jens@bluedynamics.com>
2012-10-01 18:47:37 +0200

    some pep8

--- src/soundcloudapi/__init__.py
+++ src/soundcloudapi/__init__.py
@@ -5,25 +5,25 @@ from .exceptions import SoundcloudException
 
 
 class Soundcloud(object):
-    
+
     def __init__(self, authinfo):
         self.authinfo = authinfo
-        
-    def users(self, scid=None, filter=None):
+
+    def users(self, scid=None, filter_=None):
         """A SoundCloud user
         """
-        return _api.Users(self.authinfo, scid=scid, filter=filter)    
+        return _api.Users(self.authinfo, scid=scid, filter=filter_)
 
-    def tracks(self, scid=None, filter=None):
+    def tracks(self, scid=None, filter_=None):
         """A SoundCloud track
         """
-        return _api.Tracks(self.authinfo, scid=scid, filter=filter)
-    
-    def playlists(self, scid=None, filter=None):
-        """A SoundCloud Set is internally called playlists due to some naming 
+        return _api.Tracks(self.authinfo, scid=scid, filter=filter_)
+
+    def playlists(self, scid=None, filter_=None):
+        """A SoundCloud Set is internally called playlists due to some naming
         restrictions.
         """
-        return _api.Playlists(self.authinfo, scid=scid, filter=filter)
+        return _api.Playlists(self.authinfo, scid=scid, filter=filter_)
 
     def groups(self, scid):
         """Groups have members and contributed tracks.
@@ -31,27 +31,28 @@ class Soundcloud(object):
         return _api.Groups(self.authinfo, sc=scid, filter=filter)
 
     def comments(self, scid):
-        """Comments can be made on tracks by any user who has access to a track, 
-        except for non commentable tracks. As you see in the SoundCloud player 
-        comments can also be associated with a specific timestamp in a track.
+        """Comments can be made on tracks by any user who has access to a
+        track, except for non commentable tracks. As you see in the SoundCloud
+        player comments can also be associated with a specific timestamp in a
+        track.
         """
         return _api.Comments(self.authinfo, scid)
-    
+
     def me(self):
-        """ The /me resource allows you to get information about the 
-        authenticated user and easily access his related subresources like 
+        """ The /me resource allows you to get information about the
+        authenticated user and easily access his related subresources like
         tracks, followings, followers, groups and so on.
         """
         return _api.Me(self.authinfo)
 
     def apps(self, scid):
-        """All tracks that are created and uploaded using a published app 
+        """All tracks that are created and uploaded using a published app
         include a created_with object which describes the app
         """
         return _api.Apps(self.authinfo, scid)
-    
+
     def resolve(self, url):
-        """The resolve resource allows you to lookup and access API resources 
+        """The resolve resource allows you to lookup and access API resources
         when you only know the SoundCloud.com URL
         """
         return _api.Resolve(self.authinfo)(url)
--- src/soundcloudapi/patch.py
+++ src/soundcloudapi/patch.py
@@ -5,16 +5,17 @@ from restkit.forms import (
 
 """
 Soundcloud API has a bug. This is documented at
-http://groups.google.com/group/soundcloudapi/browse_frm/thread/35498de89906c5c1?hl=en
+http://groups.google.com/group/soundcloudapi/browse_frm/thread/35498de89906c5c1
 
 Because of this we need to monkey patch restkit.
 """
 
 restkit_encode_hdr = BoundaryItem.encode_hdr
 
+
 def sc_encode_hdr(self, boundary):
     """Returns the header of the encoding of this parameter.
-    
+
     Patched for soundcloudapi
     """
     if not self._encoded_hdr or self._encoded_bdr != boundary:
@@ -27,11 +28,10 @@ def sc_encode_hdr(self, boundary):
         else:
             disposition = 'form-data; name="%s"' % self.name
         headers.append("Content-Disposition: %s" % disposition)
-        if self.filetype:
-            filetype = self.filetype
-        else:
-            pass
-            #filetype = "text/plain; charset=utf-8"
+        #if self.filetype:
+        #    filetype = self.filetype
+        #else:
+        #    filetype = "text/plain; charset=utf-8"
         #headers.append("Content-Type: %s" % filetype)
         #headers.append("Content-Length: %i" % self.size)
         headers.append("")
